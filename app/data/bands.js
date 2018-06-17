40d15b5277e58e4e060b8d8e17535bfda50e1149
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-06-16 23:42:44 -0400

    entered band data into bands.js files

---
+++ app/data/bands.js
@@ -0,0 +1,87 @@
+var bands = [
+    {
+    name: "Color Me Bad",
+    photo: "https://en.wikipedia.org/wiki/C.M.B.#/media/File:C.M.B._(Color_Me_Badd_album_-_cover_art).jpg",
+    scores:[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+]
+    },
+
+    {
+    name: "Boys II Men",
+    photo: "https://en.wikipedia.org/wiki/Full_Circle_(Boyz_II_Men_album)#/media/File:B2m-fullcircle.jpg",
+    scores:[
+        "3",
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "4",
+        "2",
+        "5",
+        "2"
+]
+    },
+
+    {
+        name: "New Kids on the Block",
+        photo: "https://en.wikipedia.org/wiki/Step_by_Step_(New_Kids_on_the_Block_album)#/media/File:Stepbystep_album_cover.jpg",
+        scores:[
+            "5",
+            "1",
+            "2",
+            "2",
+            "5",
+            "4",
+            "2",
+            "3",
+            "1",
+            "3"
+    ]
+        }, 
+
+        {
+            name: "N'Sync",
+            photo: "https://en.wikipedia.org/wiki/Greatest_Hits_(NSYNC_album)#/media/File:Dgh.JPG",
+            scores:[
+                "3",
+                "2",
+                "1",
+                "3",
+                "3",
+                "4",
+                "5",
+                "3",
+                "4",
+                "2"
+        ]
+            }, 
+            
+        {
+            name: "One Direction",
+            photo: "https://vignette.wikia.nocookie.net/onedirection/images/e/e4/1D-6.jpg/revision/latest?cb=20151226141346",
+            scores:[
+                "4",
+                "3",
+                "2",
+                "4",
+                "1",
+                "2",
+                "2",
+                "5",
+                "2",
+                "3"
+        ]
+            }    
+];
+
